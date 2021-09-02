15bef286b663cfb45f60e0f564ec6f59f63e1b73
yurigarrido <y.garrido.yuri@gmail.com>
2021-09-02 20:02:46 -0300

    ajusta o setup inicial do projeto

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import logo from './trivia.png';
+import { Route, Switch } from 'react-router-dom';
+// import logo from './trivia.png';
 import './App.css';
+import Login from './pages/Login';
+import Game from './pages/Game';
 
-export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/game" component={ Game } />
+      </Switch>
+    );
+  }
 }
+
+export default App;
