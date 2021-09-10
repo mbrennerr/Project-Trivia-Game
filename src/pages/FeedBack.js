65938617764e1dc8e4d227a1d954a69de4e38394
yurigarrido <y.garrido.yuri@gmail.com>
2021-09-10 12:02:31 -0300

    finaliza o requsito 15

--- src/pages/FeedBack.js
+++ src/pages/FeedBack.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import PlayAgain from '../component/PlayAgain';
 
 export default class FeedBack extends Component {
   render() {
     return (
       <div>
         <h1 data-testid="feedback-text">Aqui é o feedback</h1>
+        <PlayAgain />
       </div>
     );
   }
